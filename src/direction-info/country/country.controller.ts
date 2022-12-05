3181346dc1eba04c5c6fa8d60f902f4d6f9cb561
jeanca <jeancagomez94@gmail.com>
2022-12-05 09:34:59 -0500

    Creating service and controller the Country

--- src/direction-info/country/country.controller.ts
+++ src/direction-info/country/country.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Body, Param } from '@nestjs/common';
+
 import { CountryService } from './country.service';
 
 @Controller('country')
 export class CountryController {
   constructor(private readonly countryService: CountryService) {}
+
+  @Get()
+  async getCountry() {
+    const data = await this.countryService.getAll()
+    return data
+  }
+
+  @Get(':id')
+  async getCountryById(@Param('id') id: number){
+    const data = await this.countryService.getOneById(id)
+    return data
+  }
+
+  @Post('create')
+  async createCountry(@Body() data:any){
+    const country = await this.countryService.create(data)
+    return country
+  }
+
 }
--- src/direction-info/country/country.service.ts
+++ src/direction-info/country/country.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Country } from './entities/country.entity';
 
 @Injectable()
-export class CountryService {}
+export class CountryService {
+  constructor(
+    @InjectRepository(Country)
+    private readonly countryRepository: Repository<Country>,
+  ) {}
+
+  async getAll() {
+    const data = await this.countryRepository.find();
+    return data;
+  }
+
+  async getOneById(id:number){
+    const data = await this.countryRepository.findOneBy({id})
+    if(!data)return {msg:"the country does not exist"}
+    return data
+  }
+
+  async create(data: any) {
+    const exist = await this.countryRepository.findOneBy({
+      name_country: data.name_country,
+    });
+    if (!exist) {
+        const newCountry = await this.countryRepository.create(data);
+        await this.countryRepository.save(newCountry);
+        return newCountry;
+    }
+    return { msg: 'Country already exists' };
+    
+  }
+}
