b666d4b6a40329e83fc43b56537564ea6732be90
jeanca <jeancagomez94@gmail.com>
2022-12-07 13:54:55 -0500

    removing an option of services in department

--- src/direction-info/department/departament.service.ts
+++ src/direction-info/department/departament.service.ts
@@ -19,31 +19,25 @@ export class DepartamentService {
     return data;
   }
 
-  private async existCountry(id: number) {
-    const data = await this.countryService.getOneById(id);
-    return data;
-  }
-
   async getAll(): Promise<Department[] | {}> {
     const department = await this.repositoryDepart.find();
     return department;
   }
 
   async getByName(name: string): Promise<Department[] | {}> {
-    const nameToUpperCase = name.toUpperCase();
-    console.log(nameToUpperCase);
     const departament = await this.existDepart(name);
     return departament;
   }
 
   async create(data: DepartmentCreateDto) {
+
     try {
       const exist = await this.existDepart(data.name_department);
       const { countryId } = data; // destructure for verify the country
       const existCountry = await this.countryService.existCountry(countryId);
 
-      if (!exist) {
-        if (existCountry) {
+      if (existCountry) {
+        if (!exist) {
           const newDepartment = await this.repositoryDepart.create({
             ...data,
             country: { id: countryId }, //submit second parameter with the id of the country
@@ -51,9 +45,12 @@ export class DepartamentService {
           await this.repositoryDepart.save(newDepartment);
           return { msg: `Create new department -> ${data.name_department}` };
         }
-        return {msg:`the id ${countryId} country not exist`}
+
+        return { msg: `the id ${countryId} country not exist` };
       }
+
       return { msg: `Department already exists -> ${data.name_department}` };
+
     } catch (error) {
       return { msg: 'Error creating department ' + error.message };
     }
